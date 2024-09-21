4c76f818306c31b3a767967327cdb16229b3c7e6
johnai-90 <142468222+johnai-90@users.noreply.github.com>
2024-09-21 14:41:27 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.jpg" sizes="any" />
+        <link rel="icon" href="../jsm-logo.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
